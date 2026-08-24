032ef62f333fae93c1ec23f5c9a4b556a5146975
Pmredd <157102032+Pmredd@users.noreply.github.com>
2026-08-24 11:03:53 +0530

    Bump versionCode from 2 to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     applicationId = "com.aistudio.lootra.wellbeing.vfrgh"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
+    versionCode = 3
     versionName = "1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
