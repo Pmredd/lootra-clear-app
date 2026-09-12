3597de56ae6a78379d062bb13234f81f31126c0d
Pmredd <157102032+Pmredd@users.noreply.github.com>
2026-09-12 13:49:22 +0530

    Increment versionCode from 5 to 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     applicationId = "com.aistudio.lootra.wellbeing.vfrgh"
     minSdk = 24
     targetSdk = 36
-    versionCode = 5
+    versionCode = 6
     versionName = "1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
